571e0b2737342efaf0534bd1c1ec628ca932e26a
srikanthchitla <chitlasri@gmail.com>
2017-12-11 17:02:07 +0530

    Update Buffers and streams.js

--- Buffers and streams.js
+++ Buffers and streams.js
@@ -32,3 +32,20 @@ stream.on('data', function(info){
 });
 
 //  END
+
+
+
+---------------    
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+    console.log('req was made by: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+   var readStream = fs.createReadStream(__dirname + '/readFile.txt', 'utf8');
+    readStream.pipe(res);
+});
+
+server.listen(3000);
+console.log('open in browser');
+----------------------------------------------------
