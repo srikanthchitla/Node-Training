ba3fdc47b806baf76de996a8726ce6297397a14b
srikanthchitla <chitlasri@gmail.com>
2017-12-11 15:22:15 +0530

    Create Buffers and streams.js

---
+++ Buffers and streams.js
@@ -0,0 +1,8 @@
+// Buffers
+-> Temporary storage spot for a chunk of data that is being transferred from one place to another.
+-> The buffer is filled with data, then passed along.
+-> Transfer small chunks of data at a time.
+
+//streams
+-> can create a streams in node.js to transfer data.
+-> Increase performance.
