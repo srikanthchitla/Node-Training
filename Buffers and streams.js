68789ca7332b6b15de4580c9f51f21e461e969f6
srikanthchitla <chitlasri@gmail.com>
2017-12-11 16:36:04 +0530

    Update Buffers and streams.js

--- Buffers and streams.js
+++ Buffers and streams.js
@@ -6,3 +6,29 @@
 //streams
 -> can create a streams in node.js to transfer data.
 -> Increase performance.
+
+
+// writable streams
+/*
+var fs =  require('fs');
+var stream = fs.createReadStream(__dirname + '/readFile.txt', 'utf8');
+stream.on('data', function(e){
+    console.log('new chunk received');
+    console.log(e);
+}); 
+*/
+// END
+
+// writable streams
+
+var fs = require('fs');
+var stream = fs.createReadStream(__dirname + '/readFile.txt','utf8');
+var writeStream = fs.createWriteStream(__dirname + '/writeFile.txt');
+
+
+stream.on('data', function(info){
+    console.log('chunk created');
+    writeStream.write(info);
+});
+
+//  END
